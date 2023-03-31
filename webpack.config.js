9171267337e650f04ac21370d9b7daca46bcd218
Arturo Pacheco <arturojesus.pc@gmail.com>
2023-03-31 13:30:52 -0400

    script variables de entorno 4

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html',
-            filename : '/[name].[contenthash].html'
+            filename : '/index.html'
         }),        
         new MiniCssExtractPlugin({
             filename: 'assets/[name].[contenthash].css'
